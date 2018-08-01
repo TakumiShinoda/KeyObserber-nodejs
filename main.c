405a1a2ced62ee15a1251b4252940e7c3a6a4214
TakumiShinoda <godgodx4@gmail.com>
2018-08-01 20:21:09 +0900

    use json instead of variables in the code for register key

--- main.c
+++ main.c
@@ -31,13 +31,37 @@ struct KeyChecker{
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
   time_t now;
-  struct KeyChecker KeyCheckers[sizeof(Keys) / sizeof(int)];
 
-  for(int i = 0; i < sizeof(Keys) / sizeof(int); i++){
-    KeyCheckers[i].key = Keys[i];
+  std::vector<std::string> jsonKeys;
+  std::ifstream ifs("keyLayouts.json", std::ios::in);
+
+  if (ifs.fail()) {
+      std::cerr << "failed to read " << "keyLayouts.json" << std::endl;
+  }else{
+    const std::string json((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+    ifs.close();
+
+    picojson::value v;
+    const std::string err = picojson::parse(v, json);
+    if (err.empty() == false) {
+        std::cerr << err << std::endl;
+    }else{
+      picojson::object& o = v.get<picojson::object>();
+
+      for(picojson::object::const_iterator it = o.begin(); it != o.end(); it++){
+        jsonKeys.push_back((*it).first);
+      }
+    }
+  }
+
+  struct KeyChecker KeyCheckers[(int)jsonKeys.size()];
+
+  for(int i = 0; i < (int)jsonKeys.size(); i++){
+    KeyCheckers[i].key = atoi(jsonKeys[i].c_str());
     KeyCheckers[i].state = GetAsyncKeyState(KeyCheckers[i].key);
     KeyCheckers[i].get();
   }
+
 
   while(true){
     for(int i = 0; i < sizeof(Keys) / sizeof(int); i++){
